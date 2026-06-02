88527b1aa8853bbe5935c37d24e9b655c0afaca8
Kyaw Saw Aung <kyawsaw.mm@gmail.com>
2026-06-02 18:35:36 +0630

    Added Actions workflow and s3 backend

--- providers.tf
+++ providers.tf
@@ -6,4 +6,25 @@ terraform {
       version = "~> 6.46.0"
     }
   }
+
+  backend "s3" {
+    bucket       = "bootcamp-devops-backend-terraform-kyawsawww" 
+    key          = "dev/terraform.tfstate"
+    region       = "ap-southeast-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+
+  default_tags {
+    tags = {
+      project     = "prod"
+      environment = "prod" 
+      managed_by  = "terraform"
+      owner       = "bootcamp"
+    }
+  }
 }
